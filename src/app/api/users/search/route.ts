e98250101f7a2bf97cfd64c7cca8ba7e35ec2519
bilalzip <70857108+Bilalzip@users.noreply.github.com>
2023-12-20 15:30:31 +0530

    vercel

--- src/app/api/users/search/route.ts
+++ src/app/api/users/search/route.ts
@@ -1,40 +1,35 @@
 import { connect } from "@/dbConfig/dbConfig";
 import Property from "@/models/Property";
 import mongoose from "mongoose";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextResponse, response: NextResponse){
-    
-    try {
-        const Reqdata = await request.json();
-        const {data} = Reqdata;
-        console.log(data)
+export async function POST(request: NextRequest, response: NextResponse) {
+  try {
+    const Reqdata = await request.json();
+    const { data } = Reqdata;
+    console.log(data);
 
-        const properties = await Property.aggregate([
-            {
-              $search: {
-                index: "properties",
-                text: {
-                  query: data,
-                  path: {
-                    wildcard: "*",
-                  },
-                },
-              },
+    const properties = await Property.aggregate([
+      {
+        $search: {
+          index: "properties",
+          text: {
+            query: data,
+            path: {
+              wildcard: "*",
             },
-          ]);
-          
-        console.log(properties)
+          },
+        },
+      },
+    ]);
 
-        return NextResponse.json({properties});
-    
-    } catch (error:any) {
-        return NextResponse.json({
-            success:false,
-            message:error.message
-        })
-    }
-   
+    console.log(properties);
 
-
+    return NextResponse.json({ properties });
+  } catch (error: any) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
